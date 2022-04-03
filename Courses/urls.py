c1bc118a61aac402a7f5cdc9ba2fae93c04a1eaa
KrakenFragak <thiago@fragainformatica.com>
2022-04-03 11:49:57 -0300

    Added: Edit&Remove

--- Courses/urls.py
+++ Courses/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.create_course, name='create-course'),
-    # path('search/', views.retrieve_course, name='retrieve-course'),
-    # path('update/<int:pk>', views.update_course, name='update-course'),
-    # path('delete/<int:pk>', views.delete_course, name='delete-course'),
+    path('search/', views.retrieve_course, name='retrieve-course'),
+    path('update/<int:pk>', views.update_course, name='update-course'),
+    path('delete/<int:pk>', views.delete_course, name='delete-course'),
 ]
--- Courses/views.py
+++ Courses/views.py
@@ -14,3 +14,35 @@ def create_course(request):
     else:
         form = CourseForm()
     return render(request, 'create.html', {'form':form})
+
+def retrieve_course(request):
+    courses = Course.objects.all()
+    return render(request,'search.html',{'courses':courses} )
+
+def update_course(request,pk):
+    courses = Course.objects.get(id=pk)
+    form = CourseForm(instance=courses)
+
+    if request.method == 'POST':
+        form = CourseForm(request.POST, instance=courses)
+        if form.is_valid():
+            form.save()
+            return redirect('/search')
+
+    context = {
+        'courses': coursess,
+        'form': form,
+    }
+    return render(request,'update.html',context)
+
+def delete_course(request, pk):
+    courses = Course.objects.get(id=pk)
+
+    if request.method == 'POST':
+        courses.delete()
+        return redirect('/search')
+
+    context = {
+        'courses': courses,
+    }
+    return render(request, 'remove.html', context)
