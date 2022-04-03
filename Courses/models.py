5dd835619be35ccd7fc0e653ef44a09c7df12db4
KrakenFragak <thiago@fragainformatica.com>
2022-04-03 11:20:05 -0300

    Models Config

--- Courses/models.py
+++ Courses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    id = models.CharField(max_length=30)
+    Name = models.CharField(max_length=200)
+    Descr = models.CharField(max_length=300)
+    Value = models.IntField(max_length=300)
+
+    class Meta:
+        db_table = 'Course'
