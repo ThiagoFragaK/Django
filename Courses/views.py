136f6de2df95258d252017fc070373f3f4ae0304
KrakenFragak <thiago@fragainformatica.com>
2022-04-03 11:30:10 -0300

    Added: Create

--- Courses/views.py
+++ Courses/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CourseForm
+from .models import Course
 
-# Create your views here.
+def create_course(request):
+    if request.method == "POST":
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('search/')
+            except:
+                pass
+    else:
+        form = CourseForm()
+    return render(request, 'create.html', {'form':form})
