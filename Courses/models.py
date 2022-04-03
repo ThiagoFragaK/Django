120ba979521e7cdaf6ef2e1c37b3be14c2d5ea61
KrakenFragak <thiago@fragainformatica.com>
2022-04-03 11:53:48 -0300

    Fixes

--- Courses/models.py
+++ Courses/models.py
@@ -4,7 +4,7 @@ class Course(models.Model):
     CourseId = models.CharField(max_length=30)
     Name = models.CharField(max_length=200)
     Descr = models.CharField(max_length=300)
-    Value = models.IntegerField(max_length=300)
+    Value = models.IntegerField
 
     class Meta:
         db_table = 'Course'
